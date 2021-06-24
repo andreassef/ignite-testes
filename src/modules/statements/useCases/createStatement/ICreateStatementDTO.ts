3475087373a7786a6ab693db5705498aa49ffcfb
andreassef <andreassef02@gmail.com>
2021-06-24 12:35:10 -0400

    [UPDATE] - utilizacao de interface ao inves do type

--- src/modules/statements/useCases/createStatement/ICreateStatementDTO.ts
+++ src/modules/statements/useCases/createStatement/ICreateStatementDTO.ts
@@ -1,10 +1,22 @@
 import { Statement } from "../../entities/Statement";
 
-export type ICreateStatementDTO =
-Pick<
-  Statement,
-  'user_id' |
-  'description' |
-  'amount' |
-  'type'
->
+// export type ICreateStatementDTO =
+// Pick<
+//   Statement,
+//   'user_id' |
+//   'description' |
+//   'amount' |
+//   'type'
+// >
+enum OperationType {
+  DEPOSIT = 'deposit',
+  WITHDRAW = 'withdraw',
+  TRANSFER = 'transfer'
+}
+export interface ICreateStatementDTO {
+  user_id: string;
+  description: string;
+  amount: number;
+  type: OperationType;
+  sender_id?: string;
+}
